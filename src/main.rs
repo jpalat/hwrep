41a200a5a05eaf71208e3a4e62ae7ea50c5a0bfd
Jay Palat <jay@palat.net>
2020-11-07 15:48:23 -0500

    Move iec to utils.

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,10 @@ use procfs::Meminfo;
 
 mod disks;
 use disks::Disks;
-
 mod system;
 use system::CPU;
+mod utils;
+use utils::iec;
 
 fn main() {
     println!("Gathering CPU Info");
@@ -54,14 +55,7 @@ fn main() {
         );
     }
 
-    // http://stackoverflow.com/questions/5194057/better-way-to-convert-file-sizes-in-python
+   
 }
 
-pub fn iec(n: u64) -> String {
-    let units = ["", "k", "M", "G", "T", "P", "E", "Z", "Y"];
 
-    let i = (n as f64).log(1024_f64).floor() as u32;
-    let p = 1024_u64.pow(i);
-    let s = (n as f64) / (p as f64);
-    format!("{:.0}{}", s, units[i as usize])
-}
