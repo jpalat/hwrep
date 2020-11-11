6ba93fb45487be1b00c44fd09135a2975bf1e451
Jay Palat <jay@palat.net>
2020-11-11 10:16:10 -0500

    Determine the speed of the network interface.

--- src/network.rs
+++ src/network.rs
@@ -1,14 +1,15 @@
 use std::fs;
 use std::io::Error;
+use std::path::Path;
 
 #[derive(Debug)]
 pub struct Netinterface {
     pub name: String,
-    pub speed: usize,
+    pub speed: String,
 }
 
 impl Netinterface{
-    pub fn new(entry: String, speed: usize) -> Netinterface {
+    pub fn new(entry: String, speed: String) -> Netinterface {
         Netinterface{
             name: entry,
             speed,
@@ -24,13 +25,35 @@ impl Networks {
     pub fn new() -> Result<Networks, Error> {
         let sysnetwork = "/sys/class/net";
         let mut new_vec : Vec<Netinterface> = Vec::new();
+        let mut count = 0;
         for entry in fs::read_dir(sysnetwork)? {
+            count += 1;
             let entry = entry?;
 
             let path = entry.path();
             // println!("entry: {:?}",entry );
             let file_name = path.file_name().unwrap();
-            let net_int = Netinterface::new(file_name.to_str().unwrap().to_string(), 100);
+            let speed_file = Path::new("speed");
+            let new_path = path.join(speed_file);
+            let speed:String;
+            
+            match new_path.to_str() {
+                None => panic!("new path is not a valid UTF-8 sequence"),
+                Some(s) => {
+                    let contents = fs::read_to_string(new_path);
+                    match contents {
+                        Err(e) => {
+                            speed = "-".to_string();
+                            // println!("Error {}", e);
+                        },
+                        Ok(s) => speed = s,
+                    }
+                }
+            }
+
+
+            let contents = fs::read_to_string(file_name);
+            let net_int = Netinterface::new(file_name.to_str().unwrap().to_string(), speed);
             new_vec.push(net_int);
 
         }
